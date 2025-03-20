234f414f5e67e36a9849fa2a8d8d3a9891768a9c
Yairgg95 <yair.g.guadarrama@icloud.com>
2025-03-20 14:58:29 -0600

    [FEAT]Add Footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,53 @@
+import { Github, Linkedin, Mail, MessageCircle } from "lucide-react";
+export default function Footer() {
+  return (
+    <footer className="h-16 border-t-2 border-[#163874] flex flex-col items-center pb-8">
+      <a href="/" className="h-16">
+        <img
+          src="/assets/YairG-logo.png"
+          alt="YairG-logo"
+          className="h-full w-auto object-contain"
+        />
+      </a>
+
+      <h2 className="text-xl md:text-3xl  text-center font-bold bg-gradient-to-br from-[#007bff] to-[#6f42c1] text-transparent bg-clip-text">
+        Full Stack Developer
+      </h2>
+
+      <div className="flex space-x-4 mt-4">
+        <a
+          href="https://github.com/Yairgg95"
+          target="_blank"
+          className="text-gray-800 dark:text-white"
+        >
+          <Github size={24} className="hover:text-[#025CF7]" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/yair-guadarrama-gg95"
+          target="_blank"
+          className="text-gray-800 dark:text-white "
+        >
+          <Linkedin size={24} className="hover:text-[#025CF7]" />
+        </a>
+        <a
+          href="mailto:yair.g.guadarrama@gmail.com"
+          className="text-gray-800 dark:text-white"
+        >
+          <Mail size={24} className="hover:text-[#025CF7]" />
+        </a>
+        <a
+          href="https://wa.me/+525535586521"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-gray-800 dark:text-white"
+        >
+          <MessageCircle size={24} className="hover:text-[#025CF7]" />
+        </a>
+      </div>
+
+      <p className="text-sm text-gray-600 dark:text-gray-400 mt-4 mb-8">
+        © {new Date().getFullYear()} Yair. All rights reserved.
+      </p>
+    </footer>
+  );
+}
