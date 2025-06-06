2fb19683ed3d994951e9a0a9eb0849da1c4138b7
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2025-06-06 12:45:53 -0600

    [ADD] Global title tag in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import "@/styles/globals.css";
+import Head from "next/head";
 import { Toaster } from "sonner";
 
 export default function App({ Component, pageProps }) {
   return (
-  <>
-  <Component {...pageProps} />
-  <Toaster />
-  </>
+    <>
+      <Head>
+        <title>Yair Guadarrama | Full-Stack Developer & UI/UX Enthusiast</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+      <Toaster />
+    </>
   );
 }
