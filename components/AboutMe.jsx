606d9e46bebf6f5f8789162234bc4c1e2f3b05e3
Yair Guadarrama <yair.g.guadarrama@icloud.com>
2025-06-06 01:04:49 -0600

    [REFACTOR] replace h1 for Typewriter component

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { Linkedin, Github } from "lucide-react";
+import { Linkedin, Github, Mail, MessageCircle } from "lucide-react";
+import Typewriter from "./Typewriter";
 
 export default function AboutMe() {
   const [iconSize, setIconSize] = useState(32);
@@ -23,28 +24,14 @@ export default function AboutMe() {
   return (
     <section
       id="about-me"
-      className="h-full max-w-screen-2xl mx-auto flex flex-col md:flex-row items-center justify-center pt-10 md:pt-16 lg:pt-32"
+      className="h-full max-w-screen-2xl mx-auto items-center justify-center pt-10 md:pt-16 lg:pt-32"
     >
-      <div className="text-center md:text-left md:w-1/4 order-2 md:order-1 my-4 md:my-8 lg:mt-12">
-        <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-900 dark:text-white">
-          BIOGRAPHY
-        </h3>
-        <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 lg:mt-8 text-sm md:text-base lg:text-lg">
-          I transitioned into web development out of a passion for building
-          digital solutions. What started as curiosity became a drive to solve
-          problems and create impactful applications. I specialize in frontend
-          and backend technologies like JavaScript, React, Node.js, and MongoDB,
-          and I'm currently expanding my expertise in Java and MySQL to develop
-          scalable solutions.
-        </p>
-      </div>
-
-      <div className="flex flex-col items-center md:w-1/2 order-1 md:order-2 my-4 md:my-8 lg:my-12 mt-10 lg:mt-0">
-        <h1 className="text-3xl md:text-5xl  text-center font-bold bg-gradient-to-br from-[#007bff] to-[#6f42c1] text-transparent bg-clip-text">
-          Hi 👋🏼, I'm Yair <br />
-          Full Stack Developer <br />
-          Based in Mexico
-        </h1>
+      <div className="flex flex-col items-center w-full my-4 md:my-8 lg:my-12 mt-10 lg:mt-0">
+        <Typewriter
+          lines={["Hi, I'm Yair", "Full Stack Developer", "Based in Mexico"]}
+          speed={100}
+          className="text-3xl md:text-6xl 2xl:text-8xl bg-gradient-to-br from-[#007bff] to-[#6f42c1] text-transparent bg-clip-text"
+        />
 
         <div className="relative flex justify-center items-center w-48 h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 my-6 md:my-8 lg:my-10 profile-container z-10">
           <img
@@ -69,32 +56,52 @@ export default function AboutMe() {
           >
             <Linkedin size={iconSize} className="hover:text-[#025CF7]" />
           </a>
+          <a
+            href="mailto:yair.g.guadarrama@gmail.com"
+            className="text-gray-800 dark:text-white"
+          >
+            <Mail size={iconSize} className="hover:text-[#025CF7]" />
+          </a>
+          <a
+            href="https://wa.me/+525535586521"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-gray-800 dark:text-white"
+          >
+            <MessageCircle size={iconSize} className="hover:text-[#025CF7]" />
+          </a>
         </div>
       </div>
 
-      <div className="md:w-1/4 text-center md:text-left order-3 mt-4 md:my-8 lg:my-12">
-        <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-900 dark:text-white">
-          CONTACT
-        </h3>
-        <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 text-sm md:text-base lg:text-lg">
-          CDMX, Mexico.
-          <br />
-          yair.g.guadarrama@gmail.com
-          <br />
-          +52 5535586521
-        </p>
-        <h3 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 text-gray-900 dark:text-white">
-          EXPERIENCE YEARS
-        </h3>
-        <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 text-sm md:text-base lg:text-lg">
-          1+
-        </p>
-        <h3 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 text-gray-900 dark:text-white">
-          SERVICES
-        </h3>
-        <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 text-sm md:text-base lg:text-lg">
-          Frontend, Backend, Cloud Solutions (AWS, GCP)
-        </p>
+      <div className="w-full flex flex-col md:flex-row">
+        <div className="md:w-2/3 text-center md:pr-14 md:text-left my-4 md:my-8 lg:mt-12">
+          <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-900 dark:text-white">
+            BIOGRAPHY
+          </h3>
+          <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 lg:mt-8 text-sm md:text-base lg:text-lg">
+            I transitioned into web development out of a passion for building
+            digital solutions. What started as curiosity became a drive to solve
+            problems and create impactful applications. I specialize in frontend
+            and backend technologies like JavaScript, React, Node.js, and
+            MongoDB, and I'm currently expanding my expertise in Java and MySQL
+            to develop scalable solutions.
+          </p>
+        </div>
+
+        <div className="md:w-1/3 text-center md:text-left order-3 mt-4 md:my-8 lg:my-12">
+          <h3 className="text-lg md:text-xl lg:text-2xl font-semibold text-gray-900 dark:text-white">
+            EXPERIENCE YEARS
+          </h3>
+          <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 text-sm md:text-base lg:text-lg">
+            1+
+          </p>
+          <h3 className="text-lg md:text-xl lg:text-2xl font-semibold mt-4 md:mt-8 text-gray-900 dark:text-white">
+            SERVICES
+          </h3>
+          <p className="text-gray-700 dark:text-gray-300 mt-4 md:mt-6 text-sm md:text-base lg:text-lg">
+            Frontend, Backend, Cloud Solutions (AWS, GCP)
+          </p>
+        </div>
       </div>
     </section>
   );
